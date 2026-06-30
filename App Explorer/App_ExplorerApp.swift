074736a5b9077f714128897c07f349cf5a5a85b1
mumu <690481115@qq.com>
2026-06-30 19:50:15 +0800

    Initial Commit

---
+++ App Explorer/App_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ExplorerApp.swift
+//  App Explorer
+//
+//  Created by 木木 on 2026/6/30.
+//
+
+import SwiftUI
+
+@main
+struct App_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
